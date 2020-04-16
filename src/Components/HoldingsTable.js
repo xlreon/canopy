0537a9c860108821603ff46b499032abf17a51f9
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-04-16 23:00:31 +0530

    Holdings table created.

--- src/Components/HoldingsTable.js
+++ src/Components/HoldingsTable.js
@@ -1,21 +1,45 @@
 import React from "react";
 import { setTableData } from "../Actions";
 import { connect } from "react-redux";
+import "./HoldingsTable.css";
 
 class HoldingsTable extends React.Component {
   componentWillMount() {
     this.props.setTableData();
   }
 
+  _getTableColumn = (data) =>
+    Object.keys(data).map((key) => <td>{data[key]}</td>);
+
+  _getTableRow = (data) => <tr>{this._getTableColumn(data)}</tr>;
+
+  _generateTable = (data) => data.map((element) => this._getTableRow(element));
+
+  _generateTableHeaders = () => (
+    <tr>
+      {Object.keys(this.props.holdingsData[0]).map((key) => (
+        <th>{key}</th>
+      ))}
+    </tr>
+  );
+
   render() {
+    const { holdingsData } = this.props;
     return (
       <div className="card">
         <div className="card-content">
-          <p className="title">HoldingsTable</p>
+          <table style={{ height: "100%" }}>
+            {holdingsData && this._generateTableHeaders()}
+            {holdingsData && this._generateTable(holdingsData)}
+          </table>
         </div>
       </div>
     );
   }
 }
 
-export default connect(null, { setTableData })(HoldingsTable);
+const mapStateToProps = (state) => ({
+  holdingsData: state.holdingsData,
+});
+
+export default connect(mapStateToProps, { setTableData })(HoldingsTable);
